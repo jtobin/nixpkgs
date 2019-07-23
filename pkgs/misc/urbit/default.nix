9d4f0fda429ffa2bf1aa4f48d58fa1e8c96a3a48
Jared Tobin <jared@jtobin.io>
2019-07-23 20:41:58 +0900

    urbit: 0.7.4 -> 0.9.2

--- pkgs/misc/urbit/default.nix
+++ pkgs/misc/urbit/default.nix
@@ -1,31 +1,53 @@
-{ stdenv, fetchFromGitHub, curl, git, gmp, libsigsegv, meson, ncurses, ninja
-, openssl, pkgconfig, re2c, zlib
-}:
+{ stdenv, fetchurl }:
 
-stdenv.mkDerivation rec {
+let
+
   pname = "urbit";
-  version = "0.7.3";
 
-  src = fetchFromGitHub {
-    owner = "urbit";
-    repo = "urbit";
-    rev = "v${version}";
-    sha256 = "192843pjzh8z55fd0x70m3l1vncmixljia3nphgn7j7x4976xkp2";
-    fetchSubmodules = true;
+  version = "v0.9.2";
+
+  urbit-darwin = fetchurl {
+    url = "https://bootstrap.urbit.org/urbit-darwin-${version}.tgz";
+    sha256 = "0ldkxnvhw0c9gjzkn419r4p0hlja0w760ikp4pj5r7k7c02cl539";
   };
 
-  nativeBuildInputs = [ pkgconfig ninja meson ];
-  buildInputs = [ curl git gmp libsigsegv ncurses openssl re2c zlib ];
-
-  postPatch = ''
-    patchShebangs .
-  '';
-
-  meta = with stdenv.lib; {
-    description = "An operating function";
-    homepage = https://urbit.org;
-    license = licenses.mit;
-    maintainers = with maintainers; [ mudri ];
-    platforms = with platforms; linux;
+  urbit-linux64 = fetchurl {
+    url = "https://bootstrap.urbit.org/urbit-linux64-${version}.tgz";
+    sha256 = "1k3lxxy6px1in9q6nmla3577nzxmrsn09sljxnmnc6cn9i5xnryq";
   };
-}
+
+  urbit = if stdenv.isDarwin then urbit-darwin else urbit-linux64;
+
+in
+
+  stdenv.mkDerivation rec {
+    inherit pname version;
+
+    src = urbit;
+
+    unpackPhase = ''
+      mkdir -p tmp
+      tar xzf $src -C tmp
+    '';
+
+    sourceRoot = "tmp";
+
+    installPhase = ''
+      mkdir -p $out/bin
+
+      cp urbit $out/bin
+      cp urbit-worker $out/bin
+      cp -r urbit-terminfo $out/bin
+    '';
+
+    meta = with stdenv.lib; {
+      description = "A personal server operating function";
+      homepage = https://urbit.org;
+      license = licenses.mit;
+      maintainers = [ maintainers.jtobin ];
+      platforms = with platforms; linux ++ darwin;
+      # should have lower priority than when built from scratch
+      priority = 6;
+    };
+  }
+
